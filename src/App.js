312d44f5046f46f22b5b6b394496cb7de409d300
Shiyasalloor <117643544+Shiyasalloor@users.noreply.github.com>
2024-02-18 15:43:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginForm from './Components/LoginForm/LoginForm';
-import SignUpForm from './Components/SignupForm/SignupForm';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<LoginForm />} exact/>
-        <Route path='/login' element={<LoginForm />} />
-        <Route path='/signup' element={<SignUpForm />} />
-      </Routes>
-    </Router>
+    <div>
+      <LoginForm />
+    </div>
   );
 }
 
